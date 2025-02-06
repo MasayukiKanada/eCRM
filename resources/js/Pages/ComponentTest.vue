55a7ee5abaf228e5906dac0b9492ceb4e170336e
MasayukiKanada <kanada.masayuki.hata@gmail.com>
2025-02-06 16:12:18 +0900

    defineEmits

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -2,11 +2,15 @@
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import Label from '@/Components/InputLabel.vue';
 import TextInput from '@/Components/TextInput.vue';
+
+const emitTest = e => console.log(e);
 </script>
 
 <template>
 <GuestLayout>
     <Label>タイトル</Label>
-    <TextInput modelValue="初期値"></TextInput>
+    <TextInput
+    modelValue="初期値"
+    @update:model-value="emitTest"></TextInput>
 </GuestLayout>
 </template>
