ee12fda227d218726e17024b9706220640a0fb50
MasayukiKanada <kanada.masayuki.hata@gmail.com>
2025-02-06 15:06:32 +0900

    slot

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import Label from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
+</script>
+
+<template>
+<GuestLayout>
+    <Label>タイトル</Label>
+    <TextInput modelValue="初期値"></TextInput>
+</GuestLayout>
+</template>
