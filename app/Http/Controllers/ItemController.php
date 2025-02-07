a1f71d8c94fe4e3147be5c7ec7234bdc3329e0b5
MasayukiKanada <kanada.masayuki.hata@gmail.com>
2025-02-07 16:03:13 +0900

    itemsEdit

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -75,7 +75,9 @@ class ItemController extends Controller
      */
     public function edit(Item $item)
     {
-        //
+        return Inertia::render('Items/Edit', [
+            'item' => $item,
+        ]);
     }
 
     /**
