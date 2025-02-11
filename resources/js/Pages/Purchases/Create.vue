bfa6bb28179f4240d57b80bdf0cfdda05d661e5d
MasayukiKanada <kanada.masayuki.hata@gmail.com>
2025-02-11 11:17:40 +0900

    micromodal

--- resources/js/Pages/Purchases/Create.vue
+++ resources/js/Pages/Purchases/Create.vue
@@ -6,6 +6,7 @@ import { Inertia } from '@inertiajs/inertia'
 import { errorMessages } from 'vue/compiler-sfc';
 import ValidationErrors from '@/Components/ValidationErrors.vue';
 import { getToday } from '@/common';
+import MicroModal from '@/Components/Micromodal.vue';
 
 const form = reactive(
     {
@@ -90,6 +91,7 @@ const quantity = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
                                     <div class="p-2 w-full">
                                     <div class="relative">
+                                        <MicroModal />
                                         <label for="customer" class="leading-7 text-sm text-gray-600">会員名</label>
                                         <select id="customer" name="customer" v-model="form.customer_id" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                             <option v-for="customer in
