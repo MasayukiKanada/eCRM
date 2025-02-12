217f1904f263015ba18e2a514ae23c9830f9054f
MasayukiKanada <kanada.masayuki.hata@gmail.com>
2025-02-12 16:07:18 +0900

    purchaseHistoryStore

--- resources/js/Pages/Purchases/Edit.vue
+++ resources/js/Pages/Purchases/Edit.vue
@@ -15,6 +15,7 @@ const props = defineProps({
 
 const form = reactive(
     {
+        id: props.order[0].id,
         date: dayjs(props.order[0].created_at).format("YYYY-MM-DD"),
         customer_id: props.order[0].customer_id,
         status: props.order[0].status,
@@ -43,7 +44,7 @@ const totalPrice = computed(() => {
     return total
 })
 
-const storePurchase = () => {
+const updatePurchase = id => {
     itemList.value.forEach(item => {
         if(item.quantity > 0) {
             form.items.push({
@@ -52,7 +53,7 @@ const storePurchase = () => {
             })
         }
     })
-    Inertia.post(route('purchases.store'), form)
+    Inertia.put(route('purchases.update', {purchase: id}), form)
 }
 
 const quantity = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -73,7 +74,7 @@ const quantity = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
                     <div class="p-6">
                         <ValidationErrors :errors="errors"/>
                         <section class="text-gray-600 body-font relative">
-                            <form @submit.prevent="storePurchase">
+                            <form @submit.prevent="updatePurchase(form.id)">
                             <div class="container px-5 py-8 mx-auto">
                                 <div class="lg:w-1/2 md:w-2/3 mx-auto">
                                 <div class="flex flex-wrap -m-2">
@@ -137,7 +138,7 @@ const quantity = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
                                     </div>
 
                                     <div class="p-2 w-full">
-                                    <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
+                                    <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
                                     </div>
                                 </div>
                                 </div>
